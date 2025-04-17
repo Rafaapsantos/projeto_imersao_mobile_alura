dffc6c9520e741712f3f8d22af13df54857efe3e
Rafaapsantos <rafaelasantosdeveloper@gmail.com>
2025-04-17 19:48:19 -0300

    feat: creating home and splash screen

---
+++ tech_taste/lib/main.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:tech_taste/data/restaurant_data.dart';
+import 'package:tech_taste/model/restaurant.dart';
+import 'package:tech_taste/ui/_core/app_theme.dart';
+import 'package:tech_taste/ui/splash/splash_screen.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  RestaurantData restaurantData = RestaurantData();
+  await restaurantData.getRestaurants();
+  runApp(
+    MultiProvider(
+      providers: [
+        ChangeNotifierProvider(
+          create: (context) {
+            return restaurantData;
+          },
+        ),
+      ],
+    ),
+  );
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(theme: AppTheme.appTheme, home: SplashScreen());
+  }
+}
---
+++ tech_taste/lib/ui/_core/app_colors.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/animation.dart';
+
+abstract class AppColors {
+  static const Color backgroundColor = Color(0xFF202123);
+  static const Color mainClor = Color(0xFFffa559);
+  static const Color lightBackgroundColor = Color(0xFF343541);
+}
